a42e169d886f03c389e04c8a96d76260d6ed4235
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-04-06 19:48:28 -0700

    Implement UI functions on main menu and options

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using EasyTransition;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
@@ -14,6 +15,10 @@ public class MainMenu : MonoBehaviour
 
     public TransitionSettings transitionSettings;
 
+    public MusicManager musicManager;
+
+    public AudioMixer audioMixer;
+
     private void Start()
     {
         // UI Toolkit document
@@ -24,10 +29,25 @@ public class MainMenu : MonoBehaviour
         _optionsBtn = _document.rootVisualElement.Q<Button>("optionsButton");
         _quitBtn = _document.rootVisualElement.Q<Button>("quitButton");
 
+        // Hide the quit button on web (it doesn't do anything there)
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            _quitBtn.visible = false;
+        }
+
         // Setup event handlers
         _startBtn.RegisterCallback<ClickEvent>(OnStartButtonClick);
         _optionsBtn.RegisterCallback<ClickEvent>(OnOptionsButtonClick);
         _quitBtn.RegisterCallback<ClickEvent>(OnQuitButtonClick);
+
+        // Load the player preferences
+        float bgmVol = PlayerPrefs.GetFloat("BgmVol", 1f);
+        float seVol = PlayerPrefs.GetFloat("SeVol", 1f);
+
+        audioMixer.SetFloat("BGMVolume", Mathf.Log10(bgmVol) * 20);
+        audioMixer.SetFloat("SEVolume", Mathf.Log10(seVol) * 20);
+
+        musicManager.PlayMusic();
 
     }
 
@@ -35,6 +55,7 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Clicked start button");
         TransitionManager.Instance().Transition("Scenes/LevelSelect", transitionSettings, 0f);
+        musicManager.StopMusic();
     }
 
     private void OnOptionsButtonClick(ClickEvent evt)
@@ -46,5 +67,6 @@ public class MainMenu : MonoBehaviour
     private void OnQuitButtonClick(ClickEvent evt)
     {
         Debug.Log("Clicked quit button");
+        Application.Quit();
     }
 }
--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using EasyTransition;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UIElements;
 
 public class OptionsMenu : MonoBehaviour
@@ -15,6 +16,8 @@ public class OptionsMenu : MonoBehaviour
 
     public TransitionSettings transitionSettings;
 
+    public AudioMixer audioMixer;
+
     void Start()
     {
         // UI Toolkit document
@@ -24,14 +27,56 @@ public class OptionsMenu : MonoBehaviour
         _saveChangesBtn = _document.rootVisualElement.Q<Button>("saveChangesButton");
         _discardChangesBtn = _document.rootVisualElement.Q<Button>("discardChangesButton");
 
+        _bgmSlider = _document.rootVisualElement.Q<Slider>("bgmSlider");
+        _seSlider = _document.rootVisualElement.Q<Slider>("seSlider");
+
         // Add event handlers
         _saveChangesBtn.RegisterCallback<ClickEvent>(OnSaveChangesButtonClicked);
+        _discardChangesBtn.RegisterCallback<ClickEvent>(OnDiscardChangesButtonClicked);
+
+        _bgmSlider.RegisterValueChangedCallback(v =>
+        {
+            if (v.newValue == 0)
+            {
+                audioMixer.SetFloat("BGMVolume", -80f);
+            }
+            else
+            {
+                audioMixer.SetFloat("BGMVolume", Mathf.Log10(v.newValue) * 20);
+            }
+        });
+
+        _seSlider.RegisterValueChangedCallback(v =>
+        {
+            if (v.newValue == 0)
+            {
+                audioMixer.SetFloat("SEVolume", -80f);
+            }
+            else
+            {
+                audioMixer.SetFloat("SEVolume", Mathf.Log10(v.newValue) * 20);
+            }
+        });
+
+        // Init slider values
+        audioMixer.GetFloat("BGMVolume", out var bgmVal);
+        _bgmSlider.value = Mathf.Pow(10, bgmVal / 20);
+
+        audioMixer.GetFloat("SEVolume", out var seVal);
+        _seSlider.value = Mathf.Pow(10, seVal / 20);
     }
 
     private void OnSaveChangesButtonClicked(ClickEvent evt)
     {
+        PlayerPrefs.SetFloat("BgmVol", _bgmSlider.value);
+        PlayerPrefs.SetFloat("SeVol", _seSlider.value);
+        
         TransitionManager.Instance().Transition("Scenes/MainMenu", transitionSettings, 0f);
     }
 
+    private void OnDiscardChangesButtonClicked(ClickEvent evt)
+    {
+        TransitionManager.Instance().Transition("Scenes/MainMenu", transitionSettings, 0f);
+    }
 
 }
