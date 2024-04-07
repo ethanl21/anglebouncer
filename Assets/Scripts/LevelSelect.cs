b1003bd327333045e6cf28cba0d3a01c5391dfca
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2024-04-06 21:46:29 -0700

    Add empty level scenes

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -8,6 +8,11 @@ public class LevelSelect : MonoBehaviour
 {
     private UIDocument _document;
 
+    private Button _level0btn;
+    private Button _level1btn;
+    private Button _level2btn;
+    private Button _level3btn;
+
     public TransitionSettings transitionSettings;
 
     void Start()
@@ -16,10 +21,28 @@ public class LevelSelect : MonoBehaviour
         _document = GetComponent<UIDocument>();
 
         // Get references to UI components
-
+        _level0btn = _document.rootVisualElement.Q<Button>("level0Button");
+        _level1btn = _document.rootVisualElement.Q<Button>("level1Button");
+        _level2btn = _document.rootVisualElement.Q<Button>("level2Button");
+        _level3btn = _document.rootVisualElement.Q<Button>("level3Button");
 
         // Add event handlers
-
+        _level0btn.RegisterCallback((ClickEvent evt) =>
+        {
+            TransitionManager.Instance().Transition("Scenes/Levels/Level0", transitionSettings, 0f);
+        });
+        _level1btn.RegisterCallback((ClickEvent evt) =>
+        {
+            TransitionManager.Instance().Transition("Scenes/Levels/Level1", transitionSettings, 0f);
+        });
+        _level2btn.RegisterCallback((ClickEvent evt) =>
+        {
+            TransitionManager.Instance().Transition("Scenes/Levels/Level2", transitionSettings, 0f);
+        });
+        _level3btn.RegisterCallback((ClickEvent evt) =>
+        {
+            TransitionManager.Instance().Transition("Scenes/Levels/Level3", transitionSettings, 0f);
+        });
     }
 
 
